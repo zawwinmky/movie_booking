c6d64d9a71e1b53a9be49ded1b8a64bf8b741883
monkey <zawwin.zw1996@gmail.com>
2024-01-02 19:01:55 +0630

    starting otp code page

--- lib/pages/verify_sms_code_page.dart
+++ lib/pages/verify_sms_code_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_booking/pages/main_page.dart';
 import 'package:movie_booking/utils/colors.dart';
+import 'package:movie_booking/utils/dimensions.dart';
+import 'package:movie_booking/utils/strings.dart';
 
 class VerifySmsCode extends StatefulWidget {
   const VerifySmsCode({super.key});
@@ -15,14 +17,54 @@ class _VerifySmsCodeState extends State<VerifySmsCode> {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-          child: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return const MainPage();
-              }));
-            },
-            child: const Text("GO to Main Pgae")),
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kMargin22),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Image.asset(
+                kLoginLogo,
+                width: 124,
+                fit: BoxFit.fitWidth,
+              ),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return const MainPage();
+                    }));
+                  },
+                  child: Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                          child: const Text(
+                        "Confirm OTP",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                      )))),
+            ],
+          ),
+        ),
       )),
     );
   }
